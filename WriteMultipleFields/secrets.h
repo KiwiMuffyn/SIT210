fe1d556bdc88819728b5247d9184c0a3821f6e56
KiwiMuffyn <mfinmann@gmail.com>
2025-06-01 21:28:36 +1000

    Add files via upload

---
+++ WriteMultipleFields/secrets.h
@@ -0,0 +1,6 @@
+// Use this file to store all of the private credentials and detail for connections
+// replace MySSID with WiFi network name and Pass with the WiFi password
+#define SECRET_SSID ""	
+#define SECRET_PASS ""	
+#define SECRET_CH_ID 0000000			// replace 0000000 with your channel number
+#define SECRET_WRITE_APIKEY "XYZ"   // replace XYZ with your channel write API Key
